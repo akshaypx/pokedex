00a03f73a8fe8518256822ec05cdbcb5bae8eb51
akshaypx <akshaysrivastava46@gmail.com>
2024-10-28 12:05:15 +0530

    homescreen created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import { getPokedexNumber } from "./utils";
+import Header from "./components/Header";
+import SideNav from "./components/SideNav";
+import PokeCard from "./components/PokeCard";
+
 function App() {
-  return <>Hello world</>;
+  const [selectedPokemon, setSelectedPokemon] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <SideNav
+        selectedPokemon={selectedPokemon}
+        setSelectedPokemon={setSelectedPokemon}
+      />
+      <PokeCard selectedPokemon={selectedPokemon} />
+    </>
+  );
 }
 
 export default App;
